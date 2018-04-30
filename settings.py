b90487b53fa6103eb7d5785623ba3c2f23922493
GYMing <gyming@outlook.com>
2018-05-01 01:09:17 +0800

    💄 对配置文件路径做了优化

--- settings.py
+++ settings.py
@@ -3,6 +3,8 @@
 
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 """
     服务器相关设置
 """
@@ -33,8 +35,8 @@ PASSWORD = '123456789'
 """
 # 服务端的 Shadowsocks 配置文件的路径
 # 该路径是相对于本配置文件的路径，下同
-SHADOWSCOCKS_CONFIG_PATH = os.path.abspath('./shadowsocks.json')
+SHADOWSCOCKS_CONFIG_PATH = os.path.abspath(os.path.join(basedir, 'shadowsocks.json'))
 
 # 服务器生成的用户配置文件的文件夹的路径
-USER_CONFIGS_DIRECTORY_PATH = os.path.abspath('./output')
+USER_CONFIGS_DIRECTORY_PATH = os.path.abspath(os.path.join(basedir, 'output'))
 
