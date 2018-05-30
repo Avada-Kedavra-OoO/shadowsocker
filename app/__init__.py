aa862e0557a0c96ca66435eafd05ddaa47ef6420
GYMing <gyming@outlook.com>
2018-05-30 16:46:05 +0800

    🌟 添加 Email 和登录注册功能

--- app/__init__.py
+++ app/__init__.py
@@ -2,14 +2,21 @@
 # -*- coding: utf-8 -*-
 
 import logging
+import settings
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail
 from flask_apscheduler import APScheduler
+from flask_login import LoginManager
 from config import config
 from .utils import now
 
 db = SQLAlchemy()
+mail = Mail()
 scheduler = APScheduler()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'main'
 
 
 def create_app(config_name):
@@ -20,6 +27,12 @@ def create_app(config_name):
     # 注册数据库
     db.init_app(app)
 
+    # 注册 Mail
+    mail.init_app(app)
+
+    # 注册 LogIn Manager
+    login_manager.init_app(app)
+
     # 注册 APScheduler
     scheduler.init_app(app)
     scheduler.start()
@@ -28,8 +41,8 @@ def create_app(config_name):
         func='app.utils.scheduler:auto_check_ports',
         args=(app,),
         trigger='cron',
-        hour='16',
-        minute='59',
+        hour='0',
+        minute='0',
         second='0'
     )
 
